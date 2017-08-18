2276c834c4fa30c532a6727b561b535f642af7bf
Marcus Ng <marcuslistanng@gmail.com>
2017-08-18 02:36:46 -0400

    Added UIButtonExt, UIViewExt

---
+++ GoalPost/Extensions/UIButton.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton.swift
+//  GoalPost
+//
+//  Created by Marcus Ng on 8/18/17.
+//  Copyright © 2017 Marcus Ng. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0, green: 0.7768006921, blue: 0.2200525999, alpha: 1)
+    }
+    
+    func setDeselectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.6518810987, green: 0.8728772998, blue: 0.6689534783, alpha: 1)
+    }
+    
+}
